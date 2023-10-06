e3d8748ccc948b2ef92adfe07764c73f7402a645
cbalasa <c.balasa@gmail.com>
2023-10-06 08:56:21 +0300

    Clean the code a bit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { execSync } = require("child_process");
 
-let createProjectWithRealComponents = true;
-
 execSync(
 	"node ./create-vue-project-with-real-components/iterateThroughComponents.js",
 	{
@@ -12,22 +10,18 @@ execSync(
 execSync("node ./create-vue-project-with-real-components/createNewApp.js", {
 	stdio: "inherit"
 });
-// console.log("am creat app");
 execSync(
 	"node ./create-vue-project-with-real-components/installNodeModules.js",
 	{
 		stdio: "inherit"
 	}
 );
-// console.log("am instalat librariile");
 execSync("node ./create-vue-project-with-real-components/createViews.js", {
 	stdio: "inherit"
 });
-console.log("am creat paginile");
 execSync("node ./create-vue-project-with-real-components/createRouter.js", {
 	stdio: "inherit"
 });
-console.log("am creat rutele");
 execSync(
 	"node ./create-vue-project-with-real-components/copyJsonToNewWebApp.js",
 	{
@@ -38,15 +32,3 @@ execSync(
 execSync("node ./create-vue-project-with-real-components/openInVs.js", {
 	stdio: "inherit"
 });
-// console.log("am terminat");
-
-// execSync("node ./scriptsToRun/createNewApp.js");
-// console.log("am creat app");
-// execSync("node ./scriptsToRun/installNodeModules.js");
-// console.log("am instalat librariile");
-// execSync("node ./scriptsToRun/createViews.js");
-// console.log("am creat paginile");
-// execSync("node ./scriptsToRun/createRouter.js");
-// console.log("am creat rutele");
-// execSync("node ./scriptsToRun/copyJsonToNewWebApp.js");
-// console.log("am terminat");
